99f8b23b15e64cfc0b3deffbba680565ef03e9b6
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-15 18:46:34 +0530

    solve change background problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,77 @@
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 
 function App() {
+	const [color, setColor] = useState("white");
+
+	const buttons = [
+		{
+			label: "red",
+			color: "red",
+		},
+		{
+			label: "blue",
+			color: "blue",
+		},
+		{
+			label: "green",
+			color: "green",
+		},
+		{
+			label: "yellow",
+			color: "yellow",
+		},
+		{
+			label: "orange",
+			color: "orange",
+		},
+		{
+			label: "violet",
+			color: "violet",
+		},
+		{
+			label: "black",
+			color: "black",
+		},
+		{
+			label: "pink",
+			color: "pink",
+		},
+		{
+			label: "brown",
+			color: "brown",
+		},
+		{
+			label: "teal",
+			color: "teal",
+		},
+		{
+			label: "cyan",
+			color: "cyan",
+		},
+		{
+			label: "tomato",
+			color: "tomato",
+		},
+	];
+
+	const handleButtonClick = (clr) => {
+		setColor(clr);
+	};
 	return (
-		<div className="container">
+		<div className="container" style={{ background: color }}>
 			<div className="btn-box">
-				<Button />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
+				{buttons.map((btn) => {
+					return (
+						<Button
+							key={btn.label}
+							label={btn.label}
+							color={btn.color}
+							onClick={() => handleButtonClick(btn.color)}
+						/>
+					);
+				})}
 			</div>
 		</div>
 	);
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,14 @@
-const Button = () => {
-	return <button>Black</button>;
+const Button = ({ label, color, onClick }) => {
+	return (
+		<button
+			className="color-button"
+			style={{ color, borderColor: color }}
+			aria-label={`Set background to ${label}`}
+			onClick={onClick}
+		>
+			{label}
+		</button>
+	);
 };
+
 export default Button;
