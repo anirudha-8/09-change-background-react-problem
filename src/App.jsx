39c77f855fd674c05383c11b302f40c7cb349332
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-15 17:47:48 +0530

    import Button component and set up for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Button from "./components/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    
-  )
+	return (
+		<div className="container">
+			<div className="btn-box">
+				<Button />
+				<Button />
+				<Button />
+				<Button />
+				<Button />
+				<Button />
+				<Button />
+				<Button />
+				<Button />
+				<Button />
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
